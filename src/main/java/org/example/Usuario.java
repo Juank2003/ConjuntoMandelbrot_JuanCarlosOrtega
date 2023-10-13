36db2b4dcc3e1730f6afdd567965cac879943c66
Juank003 <jortealv@myuax.com>
2023-10-13 21:56:53 +0200

    ClaseUsuario

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Usuario {
+    private Mandelbrot mandelbrot;
+    private JPanel panel;
+
+    public Usuario(JPanel panel) {
+        this.panel = panel;
+        this.mandelbrot = new Mandelbrot(panel);
+    }
+
+    public void calcularMandelbrot(int numWorkers) {
+        mandelbrot.pintaMandelbrot(numWorkers);
+    }
 }
